57108a9b27f3112b1f4a3255acee3bc6c79df29d
Arthur Goldberg <artgoldberg@gmail.com>
2019-06-25 20:56:25 -0400

    add me as author to __main__

--- wc_lang/__main__.py
+++ wc_lang/__main__.py
@@ -1,6 +1,7 @@
 """ Command line programs for manipulating model definitions
 
 :Author: Jonathan Karr <karr@mssm.edu>
+:Author: Arthur Goldberg <Arthur.Goldberg@mssm.edu>
 :Date: 2016-12-07
 :Copyright: 2016, Karr Lab
 :License: MIT
