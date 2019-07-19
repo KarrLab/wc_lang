33dd71b9f56f7b346fb69baccc80957eff313f8b
Arthur Goldberg <artgoldberg@gmail.com>
2019-07-19 16:49:23 -0400

    add custom_io_classes.py file which will be used by obj_model migration of wc_lang model files

---
+++ wc_lang/migrations/custom_io_classes.py
@@ -0,0 +1,10 @@
+""" Import wc_lang's Reader so it can be used by obj_model migration of wc_lang model files
+
+:Author: Arthur Goldberg <Arthur.Goldberg@mssm.edu>
+:Date: 2019-07-18
+:Copyright: 2019, Karr Lab
+:License: MIT
+"""
+
+# this file is imported by obj_model/migrate.py
+from wc_lang.io import Reader
