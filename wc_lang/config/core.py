14e7212a4c7e2421f805ca6f096317d755b4dea0
Arthur Goldberg <artgoldberg@gmail.com>
2019-07-22 12:31:07 -0400

    move replacement for pkg_resources.resource_filename() to wc_utils and use it

--- wc_lang/config/core.py
+++ wc_lang/config/core.py
@@ -9,47 +9,9 @@
 
 import configobj
 import os
-from pathlib import Path
 import wc_utils.config
 import wc_utils.debug_logs.config
-
-
-def get_package_root(file_in_package):
-    """ Get root directory of a package
-
-    Args:
-        file_in_package (:obj:`str`): pathname of a file in a package
-
-    Returns:
-        :obj:`str`: pathname of root of package
-    """
-    path = Path(file_in_package)
-    # go up directory hierarchy from path and get first directory that does not contain '__init__.py'
-    dir = path.parent
-    found_package = False
-    while True:
-        if not dir.joinpath('__init__.py').is_file():
-            break
-        # exit at / root
-        if dir == dir.parent:
-            break
-        found_package = True
-        dir = dir.parent
-    if found_package:
-        return str(dir)
-
-
-def get_resource_filename(*args):
-    """ Get pathname of resource file; replaces `pkg_resources.resource_filename`
-
-    Args:
-        args (:obj:`list`): pathname components of resource file
-
-    Returns:
-        :obj:`str`: pathname of resource file
-    """
-    package_root = get_package_root(__file__)
-    return os.path.join(package_root, *args)
+from wc_utils.config.core import AltResourceName
 
 
 def get_config(extra=None):
@@ -59,11 +21,13 @@ def get_config(extra=None):
         extra (:obj:`dict`, optional): additional configuration to override
 
     Returns:
-        :obj:`configobj.ConfigObj`: nested dictionary with the configuration settings loaded from the configuration source(s).
+        :obj:`configobj.ConfigObj`: nested dictionary with the configuration settings loaded from the
+            configuration source(s).
     """
+    alt_resource_name = AltResourceName(__file__)
     paths = wc_utils.config.ConfigPaths(
-        default=get_resource_filename('wc_lang', 'config/core.default.cfg'),
-        schema=get_resource_filename('wc_lang', 'config/core.schema.cfg'),
+        default=alt_resource_name.resource_filename('config/core.default.cfg'),
+        schema=alt_resource_name.resource_filename('config/core.schema.cfg'),
         user=(
             'wc_lang.cfg',
             os.path.expanduser('~/.wc/wc_lang.cfg'),
@@ -118,7 +82,8 @@ def get_debug_logs_config(extra=None):
         :obj:`configobj.ConfigObj`: nested dictionary with the configuration settings loaded from the configuration source(s).
     """
     paths = wc_utils.debug_logs.config.paths.deepcopy()
-    paths.default = get_resource_filename('wc_lang', 'config/debug.default.cfg')
+    alt_resource_name = AltResourceName(__file__)
+    paths.default = alt_resource_name.resource_filename('config/debug.default.cfg')
     paths.user = (
         'wc_lang.debug.cfg',
         os.path.expanduser('~/.wc/wc_lang.debug.cfg'),
