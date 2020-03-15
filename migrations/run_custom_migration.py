a46b3f9344acd2b5a70b889b08705f06fcb5a28e
Jonathan Karr <jonrkarr@gmail.com>
2020-03-15 15:26:50 -0400

    adding all wc_sim fixtures to migration list

--- migrations/run_custom_migration.py
+++ migrations/run_custom_migration.py
@@ -36,7 +36,8 @@ paths = [
     {'path': 'wc_sim/tests/fixtures/test_dry_model_with_mass_computation.xlsx',
      'ignore_extra_models': True},
     {'path': 'wc_sim/tests/fixtures/test_dynamic_expressions.xlsx'},
-    {'path': 'wc_sim/tests/fixtures/test_model.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/test_model.xlsx',
+     'ignore_extra_models': True},
     {'path': 'wc_sim/tests/fixtures/test_model_for_access_species_populations.xlsx'},
     {'path': 'wc_sim/tests/fixtures/test_model_for_access_species_populations_steady_state.xlsx'},
     {'path': 'wc_sim/tests/fixtures/test_new_features_model.xlsx'},
@@ -54,6 +55,69 @@ paths = [
      'ignore_extra_models': True},
     {'path': 'wc_sim/tests/submodels/fixtures/test_submodel.xlsx'},
     {'path': 'wc_sim/tests/submodels/fixtures/test_submodel_no_shared_species.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/multialgorithmic/00001/00001-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/multialgorithmic/00003/00003-wc_lang_1_submodel.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/multialgorithmic/00003/00003-wc_lang_2_submodels.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/multialgorithmic/00003/00003-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/multialgorithmic/00007/00007-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/multialgorithmic/00020/00020-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/multialgorithmic/00021/00021-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/multialgorithmic/00030/00007-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/multialgorithmic/00030/00030-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/semantic/00001/00001-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/semantic/00002/00002-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/semantic/00003/00003-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/semantic/00004/00004-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/semantic/00005/00005-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/semantic/00006/00006-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/semantic/00010/00010-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/semantic/00014/00014-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/semantic/00015/00015-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/semantic/00017/00017-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/semantic/00018/00018-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/semantic/00019/00019-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/semantic/00020/00020-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/semantic/00021/00021-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/semantic/00022/00022-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/semantic/00028/00028-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/semantic/00054/00054-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/stochastic/00001/00001-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/stochastic/00003/00003-wc_lang_1_submodel.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/stochastic/00003/00003-wc_lang_2_submodels.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/stochastic/00003/00003-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/stochastic/00004/00004-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/stochastic/00007/00007-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/stochastic/00007_hybrid/00007_hybrid-wc_lang_old.xlsx',
+     'validate': False},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/stochastic/00007_hybrid/00007_hybrid-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/stochastic/00012/00012-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/stochastic/00020/00020-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/stochastic/00021/00021-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/stochastic/00030/00030-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/stochastic/00037/00037-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/cases/stochastic/transcription_translation/transcription_translation-wc_lang.xlsx',
+     'validate': False},
+    {'path': 'wc_sim/tests/fixtures/verification/testing/hybrid/transcription_translation/transcription_translation_correct_ssa.xlsx',
+     'validate': False},
+    {'path': 'wc_sim/tests/fixtures/verification/testing/hybrid/transcription_translation/transcription_translation_hybrid.xlsx',
+     'validate': False},
+    {'path': 'wc_sim/tests/fixtures/verification/testing/hybrid/transcription_translation/transcription_translation-wc_lang_JK.xlsx',
+     'validate': False},
+    {'path': 'wc_sim/tests/fixtures/verification/testing/hybrid/translation_metabolism/translation_metabolism_correct_ssa.xlsx',
+     'validate': False},
+    {'path': 'wc_sim/tests/fixtures/verification/testing/hybrid/translation_metabolism/translation_metabolism_hybrid.xlsx',
+     'validate': False},
+    {'path': 'wc_sim/tests/fixtures/verification/testing/multialgorithmic/00007/00007-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/testing/semantic/00001/00001-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/testing/semantic/00004/00004-wc_lang.xlsx',
+     'validate': False},
+    {'path': 'wc_sim/tests/fixtures/verification/testing/semantic/00054/00054-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/testing/stochastic/00001/00001-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/testing/stochastic/00006/00006-wc_lang.xlsx'},
+    {'path': 'wc_sim/tests/fixtures/verification/testing_ValidationSuite_run/stochastic/00001/00001-wc_lang.xlsx',
+     'validate': False},
+    {'path': 'wc_sim/tests/fixtures/verification/testing_ValidationSuite_run/stochastic/00006/00006-wc_lang.xlsx',
+     'validate': False},
 
     # wc_test
     {'path': 'wc_test/tests/fixtures/min_model.xlsx'},
@@ -85,12 +149,15 @@ for i_path, path in enumerate(paths):
     abs_path = os.path.join(base_dir, path['path'])
 
     # migrate
-    migration.transform(abs_path)
+    # migration.transform(abs_path)
 
     # validate
-    kwargs = copy.copy(path)
-    kwargs.pop('path')
-    try:
-        wc_lang.io.Reader().run(abs_path, **kwargs)
-    except ValueError as err:
-        warnings.warn('{} is invalid: {}'.format(path['path'], str(err)))
+    if path.get('validate', True):
+        kwargs = copy.copy(path)
+        kwargs.pop('path')
+        if 'validate' in kwards:
+            kwargs.pop('validate')
+        try:
+            wc_lang.io.Reader().run(abs_path, **kwargs)
+        except ValueError as err:
+            warnings.warn('{} is invalid: {}'.format(path['path'], str(err)))
